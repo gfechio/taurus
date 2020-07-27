013ad61a4e6d25b1312b758e163ec82020173b46
Giordano Fechio <giordano.fechio@booking.com>
2020-07-27 22:25:45 +0200

    Spliting in different files

--- src/api/main.py
+++ src/api/main.py
@@ -1,16 +1,9 @@
 from datetime import datetime 
 from flask import flash, redirect, render_template, url_for
-from flask import Flask, Blueprint, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
 from flask_restx import Api, Namespace, Resource, fields
 
-import sqlalchemy.orm
-from cockroachdb.sqlalchemy import run_transaction
-
 app = Flask(__name__)
-app.config.from_pyfile('api.cfg')
-db = SQLAlchemy(app)
-sessionmaker = sqlalchemy.orm.sessionmaker(db.engine)
 api = Api(
     app,
     base_url='/api/',
@@ -21,62 +14,13 @@ api = Api(
     description='Taurus API.'
 )
 
-ns = api.namespace('/api', description='Taurus Operations.')
+ns = api.namespace('', description='Taurus Operations.')
 
-class Todo(db.Model):
-    __tablename__ = "todos"
-    id = db.Column("todo_id", db.Integer, primary_key=True)
-    title = db.Column(db.String(60))
-    text = db.Column(db.String)
-    done = db.Column(db.Boolean)
-    pub_date = db.Column(db.DateTime)
+from todo import api as todo
+from stocks import api as stocks
 
-    def __init__(self, title, text):
-        self.title = title
-        self.text = text
-        self.done = False
-        self.pub_date = datetime.utcnow()
-
-
-
-@ns.route('/todo/')
-class Todo(Resource):
-    def get():
-        return render_template(
-            "show_all.html", todos=Todo.query.order_by(Todo.pub_date.desc()).all()
-        )
-    def post():
-        if not request.form["title"]:
-            flash("Title is required", "error")
-        elif not request.form["text"]:
-            flash("Text is required", "error")
-        else:
-            todo = Todo(request.form["title"], request.form["text"])
-            db.session.add(todo)
-            db.session.commit()
-            flash("Todo item was successfully created")
-            return redirect(url_for("show_all"))
-
-        return render_template("new.html")
-
-    def put():
-        for todo in Todo.query.all():
-            todo.done = f"done.{todo.id}" in request.form
-        flash("Updated status")
-        db.session.commit()
-        return redirect(url_for("show_all"))
-
-@ns.route('/stocks')
-@ns.doc('Manage Stocks list to check.')
-@ns.param('stock', 'AAPL', location='arg')
-class Stocks(Resource):
-    def get(self):
-        stocks = request.args.get('stocks')
-        if stocks is None:
-            msg = 'The "stock" parameter is required.'
-            api.abort(400, msg)
-
-        return jsonify(stocks)
+api.add_namespace(todo)
+api.add_namespace(stocks)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- src/api/todo.py
+++ src/api/todo.py
@@ -1,25 +1,55 @@
-class TodoDAO(object):
-    def __init__(self):
-        self.counter = 0
-        self.todos = []
+from datetime import datetime 
+from flask import flash, redirect, render_template, url_for
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api, Namespace, Resource, fields
 
-    def get(self, id):
-        for todo in self.todos:
-            if todo['id'] == id:
-                return todo
-        api.abort(404, "Todo {} doesn't exist".format(id))
+import sqlalchemy.orm
+from cockroachdb.sqlalchemy import run_transaction
 
-    def create(self, data):
-        todo = data
-        todo['id'] = self.counter = self.counter + 1
-        self.todos.append(todo)
-        return todo
+todo = Flask(__name__)
+todo.config.from_pyfile('api.cfg')
+db = SQLAlchemy(todo)
+sessionmaker = sqlalchemy.orm.sessionmaker(db.engine)
+api = Namespace('api/todo', description='Taurus ToDo Operations.')
 
-    def update(self, id, data):
-        todo = self.get(id)
-        todo.update(data)
-        return todo
+class Todo(db.Model):
+    __tablename__ = "todos"
+    id = db.Column("todo_id", db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    text = db.Column(db.String)
+    done = db.Column(db.Boolean)
+    pub_date = db.Column(db.DateTime)
 
-    def delete(self, id):
-        todo = self.get(id)
-        self.todos.remove(todo)
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.done = False
+        self.pub_date = datetime.utcnow()
+
+@api.route('/')
+class Todo(Resource):
+    def get():
+        return render_template(
+            "show_all.html", todos=Todo.query.order_by(Todo.pub_date.desc()).all()
+        )
+    def post():
+        if not request.form["title"]:
+            flash("Title is required", "error")
+        elif not request.form["text"]:
+            flash("Text is required", "error")
+        else:
+            todo = Todo(request.form["title"], request.form["text"])
+            db.session.add(todo)
+            db.session.commit()
+            flash("Todo item was successfully created")
+            return redirect(url_for("show_all"))
+
+        return render_template("new.html")
+
+    def put():
+        for todo in Todo.query.all():
+            todo.done = f"done.{todo.id}" in request.form
+        flash("Updated status")
+        db.session.commit()
+        return redirect(url_for("show_all"))
