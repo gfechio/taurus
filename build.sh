2708d36c8b6b131e4b684bf87d8b5a0339d2f8f6
Giordano Fechio <giordano.fechio@booking.com>
2020-08-17 09:16:22 +0200

    Adjusting builds

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-docker build -t $USERNAME/taurus:api app/api/ --push
-docker build -t $USERNAME/taurus:worker app/worker/ --push
-docker build -t $USERNAME/taurus:searcher app/searcher/ --push
+docker build --platform linux/arm/v7 -t $USERNAME/taurus:api app/api/ 
+docker push $USERNAME/taurus:api
+docker build --platform linux/arm/v7 -t $USERNAME/taurus:worker app/worker/ 
+docker push $USERNAME/taurus:worker
+docker build --platform linux/arm/v7 -t $USERNAME/taurus:searcher app/searcher/ 
+docker push $USERNAME/taurus:searcher
